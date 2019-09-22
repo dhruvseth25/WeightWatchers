d4b973be740244f2d66a1d63d1a53967237f8c43
Dhruv Seth <[dhruvseth25@gmail.com]>
2019-09-22 17:42:56 -0400

    Update Question3.java

--- Question3/Question3.java
+++ Question3/Question3.java
@@ -1,5 +1,3 @@
-package com.testCases;
-
 import java.util.Random;
 import java.util.Scanner; 
 import java.util.Arrays; 
@@ -7,28 +5,23 @@ import java.util.Arrays;
 
 public class Question3 {
 		
-		  public static void main(String[] args) {
-		    Random rand = new Random(); 
-		   int i;
-		        Scanner input = new Scanner(System.in); 
-		        final int N = 500;
-		        int array[]  = new int [N];
-		        for(i=0;i<array.length;i++)
-		        {
-		            array[i] = rand.nextInt(500);
-		            System.out.println("No:  "+array[i]);
-		        }
-		        System.out.println();
-		        System.out.print("Enter your choice: ");
-		        int n  = input.nextInt();
-		        System.out.println(findNthSmallest(array, n));
-		  }
-		  public static int findNthSmallest(int[] array, int n) 
-		  {
-		    Arrays.sort(array);
-		    return array[n-1];
-		  }	  
-		
+	public static void main(String[] args) {
+		Random rand = new Random(); 
+		int i;
+		Scanner input = new Scanner(System.in); 
+		final int N = 500;
+		int array[]  = new int [N];
+		for(i=0;i<array.length;i++) {
+			array[i] = rand.nextInt(500);
+		}
+		System.out.println("500 random numbers generated.");
+		System.out.print("Enter the value of n (to find n'th smallest number): ");
+		int n  = input.nextInt();
+		System.out.println(n + "th smallest no is " + findNthSmallest(array, n));
 	}
-
-
+	public static int findNthSmallest(int[] array, int n){
+		Arrays.sort(array);
+		return array[n-1];
+	}	  
+		
+}
